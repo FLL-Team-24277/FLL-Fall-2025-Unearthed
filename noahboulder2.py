62d6dcaf5fd1a8b4cdeaa33f1ae06adefcf18cc9
noah <nbernstein2031@norfolkcollegiate.org>
2025-10-08 16:51:43 -0400

    lalala boGINI

--- noahboulder2.py
+++ noahboulder2.py
@@ -10,17 +10,17 @@ from base_robot import *
 def Run(br: BaseRobot):
     # Your mission code goes here, step-by-step
     # It MUST be indented just like the lines below
-    br.moveLeftAttachmentMotorForMillis(millis=350, speedPct=-80, waiting=False)
+    br.moveLeftAttachmentMotorForMillis(millis=350, speedPct=40, waiting=False)
     br.driveArcDist(radius=100, dist=-200, speedPct=80, then=Stop.BRAKE, waiting=True)
     # Stop.NONE
     br.driveForDistance(distance=-50, speedPct=80, then=Stop.BRAKE, waiting=True)
-    br.moveRightAttachmentMotorForDegrees(degrees=150, speedPct=80, waiting=False)
+    # br.moveRightAttachmentMotorForDegrees(degrees=150, speedPct=80, waiting=False)
     br.driveForDistance(distance=600, speedPct=80, then=Stop.BRAKE, waiting=True)
     br.turnInPlace(angle=30, speedPct=40)
-    br.driveForDistance(distance=195, speedPct=80, then=Stop.BRAKE, waiting=True)
-    br.moveRightAttachmentMotorForMillis(millis=1000, speedPct=80)
-    br.driveForDistance(distance=50, speedPct=80, then=Stop.BRAKE, waiting=True)
-    br.moveLeftAttachmentMotorForMillis(millis=600, speedPct=40)
+    br.driveForDistance(distance=105, speedPct=80, then=Stop.BRAKE, waiting=True)
+    # br.moveRightAttachmentMotorForMillis(millis=1000, speedPct=80)
+    # br.driveForDistance(distance=50, speedPct=80, then=Stop.BRAKE, waiting=True)
+    br.moveLeftAttachmentMotorForMillis(millis=600, speedPct=-40)
     # br.waitForMillis(millis=800)
     # br.moveLeftAttachmentMotorForMillis(millis=1000, speedPct=-80)
     # br.driveForDistance(distance=100, speedPct=80, then=Stop.BRAKE, waiting=True)
@@ -28,11 +28,11 @@ def Run(br: BaseRobot):
     # br.driveForDistance(distance=-100, speedPct=80, then=Stop.BRAKE, waiting=True)
     # br.turnInPlace(angle=-50, speedPct=45,waiting=True)
     # br.moveRightAttachmentMotorForMillis(millis=400, speedPct=-60)
-    # br.waitForMillis(millis=950)
-    # br.driveForDistance(distance=-200, speedPct=80, then=Stop.BRAKE, waiting=True)
-    # br.turnInPlace(angle=-40, speedPct=45)
-    # br.driveArcDist(radius=300, dist=-200, speedPct=80, then=Stop.BRAKE, waiting=True)
-    # br.driveForDistance(distance=-700, speedPct=80, then=Stop.BRAKE, waiting=True)
+    br.waitForMillis(millis=950)
+    br.driveForDistance(distance=-200, speedPct=80, then=Stop.BRAKE, waiting=True)
+    br.turnInPlace(angle=-40, speedPct=45)
+    br.driveArcDist(radius=300, dist=-200, speedPct=80, then=Stop.NONE, waiting=True)
+    br.driveForDistance(distance=-700, speedPct=80, then=Stop.BRAKE, waiting=True)
 # Leave everything below here and don't type anything below this line
 # If running this program directly (not from the master program), this is
 # how we know it is running directly. In which case, this method will
