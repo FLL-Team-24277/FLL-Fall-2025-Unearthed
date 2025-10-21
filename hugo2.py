65b4d077d70b368866a34189bbde869e6be79c10
Hugo <hhanenburg2032@norfolkcollegiate.org>
2025-10-21 16:50:29 -0400

    now I'm done

--- hugo2.py
+++ hugo2.py
@@ -22,8 +22,8 @@ def Run(br: BaseRobot):
         radius=-230, angle=-180, speedPct=80, then=Stop.BRAKE, waiting=True
     )
     br.driveForDistance(distance=-5, speedPct=10, then=Stop.BRAKE, waiting=True)
-    br.driveForDistance(distance=20, speedPct=80, then=Stop.BRAKE, waiting=True)
-    br.moveRightAttachmentMotorForMillis(millis=2000, speedPct=-25)
+    br.driveForDistance(distance=35, speedPct=80, then=Stop.BRAKE, waiting=True)
+    br.moveRightAttachmentMotorForMillis(millis=2500, speedPct=-20)
     br.driveForDistance(
        distance=300, speedPct=80, then=Stop.BRAKE, waiting=True
     )
