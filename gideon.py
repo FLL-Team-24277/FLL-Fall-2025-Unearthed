74628be1c87a77222007b55fd754f717358715a4
hiddenviper123 <ghoffman2032@norfolkcollegiate.org>
2025-11-06 17:00:18 -0500

    update

--- gideon.py
+++ gideon.py
@@ -25,16 +25,16 @@ def Run(br: BaseRobot):
     br.driveForDistance(
         distance=60, speedPct=25, then=Stop.BRAKE, waiting=True
     )
-    for pushes in range(2):
+    for pushes in range(3):
         # br.driveForDistance(
         #     distance=dist, speedPct=100, then=Stop.BRAKE, waiting=True, accelerationPct=100
         # )
         br.driveForDistance(
-            distance=dist + 20,
+            distance=dist + 30,
             speedPct=100,
             then=Stop.BRAKE,
             waiting=True,
-            accelerationPct=20,
+            accelerationPct=7,
         )
         br.driveForDistance(
             distance=-dist,
