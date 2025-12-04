a024c07f8c974525cc0761941ce9dbda0d459c98
NOT Declan <dwhitehead2030@norfolkcollegiate.org>
2025-12-04 14:27:18 -0800

    updates

--- not_declan_not_brushy.py
+++ not_declan_not_brushy.py
@@ -8,11 +8,17 @@ from base_robot import *
 # When we run this program from the master program, we will call this
 # "Run(br)" method.
 def Run(br: BaseRobot):
-    br.driveForDistance(distance=350, speedPct=80, then=Stop.BRAKE, waiting=True)
-    br.driveForDistance(distance=-100,speedPct=100)
-    br.driveForDistance(distance=200,speedPct=45)
+    br.driveForDistance(
+        distance=350, speedPct=80, then=Stop.BRAKE, waiting=True
+    )
+    br.driveForDistance(distance=-100, speedPct=100)
+    br.driveForDistance(distance=200, speedPct=45)
     br.moveLeftAttachmentMotorForMillis(millis=1000, speedPct=80)
-    br.driveForDistance(distance=-400, speedPct=80, then=Stop.BRAKE, waiting=True)
+    br.driveForDistance(
+        distance=-400, speedPct=80, then=Stop.BRAKE, waiting=True
+    )
+
+
 # Leave everything below here and don't type anything below this line
 # If running this program directly (not from the master program), this is
 # how we know it is running directly. In which case, this method will
